1d957fdf60f8b5a05c0f6540e1e9a76775e30198
ayandele <adele.ayansola@yahoo.com>
2023-09-11 12:15:52 -0400

    server.js code update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const express = require('express');
+// Import the ApolloServer class
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 
-
-
-
-
-
-
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
