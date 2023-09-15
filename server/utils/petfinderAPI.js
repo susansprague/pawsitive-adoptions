d1b8e13cff48990c0721ebdae8792169cea1502a
Noah Salmon <noahsalmon98@gmail.com>
2023-09-14 21:02:23 -0400

    fixed petfinderapi

--- server/utils/petfinderAPI.js
+++ server/utils/petfinderAPI.js
@@ -1,20 +1,27 @@
-const axios = require('axios');
+const axios = require ('axios');
 
 // Function to fetch pets from the Petfinder API
 async function fetchPets() {
   try {
+
+    const token = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
+      grant_type: 'client_credentials',
+      client_id: 'DT75AaLbIi1adGvjezFxg76jPv7q0eD8la9gTYTRJUwwqD4Zeg',
+      client_secret: 'NKvFY0T5atFlX8LIsx15d3XImDEYhDSodqQfAQrF'
+    })
+
+console.log(token)
     const response = await axios.get('https://api.petfinder.com/v2/animals', {
-      params: {
+
         // Include API key in the request headers
         headers: {
-          'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}`,
-        },
-      },
+          'Authorization': `Bearer ${token.data.access_token}`,
+        }
     });
+    console.log(response)
     return response.data;
   } catch (error) {
     throw new Error(error.message);
   }
 }
-
 module.exports = { fetchPets };
